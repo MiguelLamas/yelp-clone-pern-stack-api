5fb1cf65024523c009954f3ebc95acccbda33399
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-07-06 20:16:58 +0100

    Added dotenv to DB and DATABASE_URL

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,6 @@
 // importing and destructuring Pool from pg;
 const { Pool } = require("pg");
+require("dotenv").config();
  
 // using Pool to connect with our Postgres DB;
 const pool = new Pool({
@@ -8,10 +9,15 @@ const pool = new Pool({
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
 });
+
+
  
 // exporting query function;
 module.exports = {
     query: (text, params) => pool.query(text, params),
 
 }
+
+
