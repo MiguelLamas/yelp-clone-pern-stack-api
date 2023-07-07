50ac62b169b668100d7db6940ce59c53721606b6
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-07-07 23:56:46 +0100

    index.js db amended

--- server/db/index.js
+++ server/db/index.js
@@ -2,23 +2,24 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const databaseConfig = {
-    connectionString: process.env.DATABASE_URL
-};
+
+const devConfig = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT,
+  };
+
+  const proConfig = {
+    connectionString: process.env.DATABASE_URL,
+  };
  
 // using Pool to connect with our Postgres DB;
-const pool = new Pool(databaseConfig);
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
     
-// ({
-//     user: process.env.PG_USER,
-//     host: process.env.PG_HOST,
-//     database: process.env.PG_DATABASE,
-//     password: process.env.PG_PASSWORD,
-//     port: process.env.PG_PORT,
-// });
-
-
- 
 // exporting query function;
 module.exports = {
     query: (text, params) => pool.query(text, params),
