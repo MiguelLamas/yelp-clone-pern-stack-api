3b6b4d6db1faa9722c6ef74fd372fd7429f9d450
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-07-11 18:37:11 +0100

    reinstated app.use function

--- index.js
+++ index.js
@@ -25,18 +25,18 @@ client.connect(function(err) {
 
 // middlewear
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Methods", 
-//   "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-//   );
-//   res.header
-//   ("Access-Control-Allow-Headers", 
-//   "Origin, X-Requested-With, Content_Type, Accept, Authorization"
-//   );
-//   next();
-// })
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods", 
+  "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header
+  ("Access-Control-Allow-Headers", 
+  "Origin, X-Requested-With, Content_Type, Accept, Authorization"
+  );
+  next();
+})
 app.use(express.json());
 
 
